6dd331ea4b3ff3c239517b5e635d396540b540cb
oscar-abud <oscarpalma.vpr2@gmail.com>
2026-06-02 14:13:35 -0400

    [FEAT] Add boostratp and toast to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// 1. Importar el CSS de Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+// 2. Importar el CSS de React-Toastify
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import 'react-toastify/dist/ReactToastify.css' 
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
